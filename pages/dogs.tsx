afe6d2e88f9039db9671d57404fdffad65cfd3f9
itsBalbasaur <126829736+itsBalbasaur@users.noreply.github.com>
2023-03-02 15:18:19 -0800

    Update dogs.tsx

--- pages/dogs.tsx
+++ pages/dogs.tsx
@@ -1,21 +1,40 @@
-import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import Header from "@/components/Header";
-import Card from "@/components/Card";
+import Head from 'next/head';
+import styles from '@/styles/Home.module.css';
+import Header from '@/components/Header';
+import Card from '@/components/Card';
+import { GetStaticProps } from 'next';
+import { fetchDogBreeds, fetchDogBreedImage } from '@/lib/dogs.service';
 
-export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>My Dog App</title>
-				<meta name="description" content="my first dog app" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main className={styles.main}>
-				<Header>Header Component</Header>
-				<Card>Card Component</Card>
-			</main>
-		</>
-	);
+export default function Home({ breeds }) {
+  return (
+    <>
+      <Head>
+        <title>My Dog App</title>
+        <meta
+          name='description'
+          content='my first dog app'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
+        <link
+          rel='icon'
+          href='/favicon.ico'
+        />
+      </Head>
+      <main className={styles.main}>
+        <Header>Header Component</Header>
+        <Card>Card Component</Card>
+      </main>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async context => {
+  let results = fetchDogBreeds;
+
+  return {
+    props: { breeds: results },
+  };
+};

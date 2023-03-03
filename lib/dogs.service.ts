64fb51b4b028b29a7ff0640d3d22e1aa1f7fd99a
itsBalbasaur <126829736+itsBalbasaur@users.noreply.github.com>
2023-03-03 10:27:55 -0800

    Update dogs.service.ts

--- lib/dogs.service.ts
+++ lib/dogs.service.ts
@@ -2,6 +2,12 @@ export type DogBreeds = {
   [breed: string]: string[];
 };
 
+// export const fetchDogBreeds = async () => {
+//   const res = await fetch('https://dog.ceo/api/breeds/list/all');
+//   const json = await res.json();
+//   return json.message as DogBreeds;
+// };
+
 export const fetchDogBreeds = async () => {
   const res = await fetch('https://dog.ceo/api/breeds/list/all');
   const json = await res.json();
@@ -27,3 +33,16 @@ export const fetchDogBreedImage = async (breed: string, subbreed?: string) => {
   return json.message as string;
 };
 
+// export const fetchDogBreeds = async () => {
+//   return fetch('https://dog.ceo/api/breeds/list/all')
+//     .then(resp => resp.json())
+//     .then(data => data.message as DogBreeds);
+// };
+
+// export const fetchDogBreedImage = async (breed: string, subbreed?: string) => {
+//   let query = breed;
+//   if (subbreed) query += `/${subbreed}`;
+//   return fetch(`https://dog.ceo/api/breed/${query}/images/random`)
+//     .then(resp => resp.json())
+//     .then(data => data.message as string);
+// };
